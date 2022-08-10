88525cafe4cd5f9fcec64675829e6bea1c771bb7
naveenselvarasu <1998naveens@gmail.com>
2022-08-10 11:45:26 +0530

    adding function

--- ProductApp/views.py
+++ ProductApp/views.py
@@ -122,6 +122,20 @@ def delete_product(request):
     else:
         return Response(data ="you are data is not already exist")  
         
+        
+        
+
+# delete view 
+
+@api_view(["POST"])
+def delete_product_group(request):
+    data = request.data
+    if ProductGroup.objects.all().filter(type = data["type"]).exists():
+        ProductGroup.objects.all().filter(type = data["type"]).delete()
+        Product.objects.all().filter(productgroup = data["type"]).delete()
+        return Response(data ="you are successfully deleted obj")
+    else:
+        return Response(data ="you are data is not already exist")
 
  
 
